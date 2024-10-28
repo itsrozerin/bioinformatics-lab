9db5bb571ba994143c8d67e9717e0f400a90d1e1
rosemary <137038969+itsrozerin@users.noreply.github.com>
2024-10-28 03:40:19 +0100

    Update needleman_wunsch.py

--- Assignment#1/src/needleman_wunsch.py
+++ Assignment#1/src/needleman_wunsch.py
@@ -1,4 +1,5 @@
 import csv
+import numpy
 
 def read_substitution_matrix(filepath):
     substitution_matrix = {}
@@ -9,3 +10,11 @@ def read_substitution_matrix(filepath):
             pair = (headers[i+1], headers[j+1])
             substitution_matrix[pair] = int(score)
         return substitution_matrix
+    
+
+def needleman_wunsch(seq1, seq2, n, substitution_matrix_path, gap_penalty, output_file):
+    substitution_matrix = read_substitution_matrix(substitution_matrix_path)
+    len_seq1, len_seq2 = len(seq1), len(seq2)
+    dp_matrix = np.zeros((len_seq1 + 1, len_seq2 + 1))
+
+    
